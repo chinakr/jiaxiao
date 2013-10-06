369a03f786a4f717ffd1394219d5fe76d4ddaae8
chinakr <chnakr@gmail.com>
2013-10-07 00:04:59 +0800

    首页设置标题。

--- jiaxiao/jiaxiao/config.py
+++ jiaxiao/jiaxiao/config.py
@@ -5,7 +5,7 @@
 site_name = u'海淀驾校'
 
 channels = [
-    {'name': u'驾校简介', 'name_en': 'introduction'},
+    {'name': u'驾校简介', 'name_en': 'introduction', 'title': u'北京市海淀区汽车驾驶学校'},
     {'name': u'学车价格', 'name_en': 'price'},
     {'name': u'学车流程', 'name_en': 'procedure'},
     {'name': u'班车路线', 'name_en': 'shuttlebus'},
